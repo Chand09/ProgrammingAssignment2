b5affcb3a24130dacc993894287603d4f0de108a
Chand09 <40521926+Chand09@users.noreply.github.com>
2018-06-24 19:45:56 +0530

    Update cachematrix.R
    Programming Assignment 2 of the R programming course on Coursera - Lexical Scoping - Cached inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,13 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inverse<-NULL
+ set<-function(y) {
+ x<<-y
+ inverse<<-NULL }
+ setInv<-function(inv) inverse<<-inv
+ getInv<-function() inverse
+ list(set=set, get=get, setInv=setInv, getInv=getInv) }
 }
 
 
@@ -12,4 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inverse<-x$getInv()
+if(!is.null(inverse)) {
+message("Retrieving data from Cache")
+return(inverse) }
+data<-as.vector(x$get()) ##data variable assigned as a vector and will be converted to matrix inside solve function
+len=sqrt(length(data)) ## len will be used to set the number of rows for the square matrix.
+inverse<- solve(matrix(data,nrow=len))
+x$setInv(inverse)
+inverse }
 }
